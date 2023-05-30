12d6944561358275eec6880052cbd2dbcdbe04d6
sathwik-14 <kywagle@gmail.com>
2023-05-30 10:29:12 +0530

    format was unsupported and changed it to supporting one.

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,7 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  const isoDateString = new Date(dateString).toISOString(); // Ensure dateString is a valid JavaScript Date object
+  const date = parseISO(isoDateString);
+  return <time dateTime={isoDateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
