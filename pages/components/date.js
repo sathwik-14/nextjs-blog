85e4a491be9f5dc2116c5a8cbe18c7654b7e7897
sathwik-14 <kywagle@gmail.com>
2023-05-30 10:32:13 +0530

    fixed now.

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,13 @@
+// import { parseISO, format } from 'date-fns';
+
+// export default function Date({ dateString }) {
+//   const date = parseISO(dateString);
+//   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+// }
+
 import { parseISO, format } from 'date-fns';
 
-export default function Date({ dateString }) {
+export default function Dates({ dateString }) {
   const isoDateString = new Date(dateString).toISOString(); // Ensure dateString is a valid JavaScript Date object
   const date = parseISO(isoDateString);
   return <time dateTime={isoDateString}>{format(date, 'LLLL d, yyyy')}</time>;
