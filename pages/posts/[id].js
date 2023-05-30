44bbc454d9da6ab15c4c3e2df197f1c7a9765113
sathwik-14 <kywagle@gmail.com>
2023-05-30 10:17:07 +0530

    final commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
+import styles from '../../styles/Home.module.css';
+import Link from 'next/link';
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+  
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+
+  export default function Post({ postData }) {
+    return (
+      <div className={utilStyles.container}>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      </div>
+    );
+  }
