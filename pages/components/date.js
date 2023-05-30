d00719da1f3a4f152091c385cbb62762ed797498
sathwik-14 <kywagle@gmail.com>
2023-05-30 10:37:33 +0530

    .

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,14 +1,14 @@
-// import { parseISO, format } from 'date-fns';
-
-// export default function Date({ dateString }) {
-//   const date = parseISO(dateString);
-//   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-// }
-
 import { parseISO, format } from 'date-fns';
 
-export default function Dates({ dateString }) {
-  const isoDateString = new Date(dateString).toISOString(); // Ensure dateString is a valid JavaScript Date object
-  const date = parseISO(isoDateString);
-  return <time dateTime={isoDateString}>{format(date, 'LLLL d, yyyy')}</time>;
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
+
+// import { parseISO, format } from 'date-fns';
+
+// export default function Dates({ dateString }) {
+//   const isoDateString = new Date(dateString).toISOString(); // Ensure dateString is a valid JavaScript Date object
+//   const date = parseISO(isoDateString);
+//   return <time dateTime={isoDateString}>{format(date, 'LLLL d, yyyy')}</time>;
+// }
